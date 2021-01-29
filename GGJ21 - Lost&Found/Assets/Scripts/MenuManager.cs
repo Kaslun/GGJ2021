2912e65152eca9fd8344cb3a795149e0d4f6fd38
Kasper Lunde <kasperml@hotmail.com>
2021-01-29 20:36:56 +0100

    Added TextMeshProUGUI, Created MenuManager and GameManager

---
+++ GGJ21 - Lost&Found/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] menus;
+
+    [SerializeField]
+    int currentMenu = 0;
+
+    private void Awake()
+    {
+        SwitchMenu(0);
+    }
+
+    public void SwitchMenu(int newMenu)
+    {
+        foreach(GameObject o in menus)
+        {
+            o.SetActive(false);
+        }
+
+        menus[newMenu].SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
