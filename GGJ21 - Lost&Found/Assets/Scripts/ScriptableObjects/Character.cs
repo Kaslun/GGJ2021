28fb5e5e5ffcfc766a81a28804f7a1afa067d069
Kasper Lunde <kasperml@hotmail.com>
2021-01-29 21:05:59 +0100

    Tweaked UI

---
+++ GGJ21 - Lost&Found/Assets/Scripts/ScriptableObjects/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "NPC/Character")]
+public class Character : ScriptableObject
+{
+    public RawImage avatar;
+}
